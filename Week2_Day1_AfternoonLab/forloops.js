429c6ad197172bd650c4b0ae5b863eb79306ab22
Kevin Zhong <kevinzhongny@gmail.com>
2020-11-23 12:57:34 -0500

    week2_day2_morninglab

---
+++ Week2_Day1_AfternoonLab/forloops.js
@@ -0,0 +1,22 @@
+// Slide 21
+
+let symbol = '';
+for (let i= 1; i < 8; i++) {
+    symbol += '#';
+    console.log(symbol);
+}
+
+// Slide 22
+
+for (let a = 10; a > 0; a--) {
+    console.log(a);
+}
+
+// Slide 23 replacing var/let returns a TypeError
+
+// Slide 24
+
+for (let b = 6; b < 61; b = b+3) {
+    console.log(b);
+}
+
---
+++ Week2_Day2_MorningLab/assignment.js
@@ -0,0 +1,47 @@
+//  1. Using a while loop, print out all prime numbers between 0 - 20
+
+let prime = 1;
+while (prime < 20) {
+    let test = 2;
+    while (prime >= test) {
+        if (prime % test === 0 && prime != test) {
+            break;
+        } else if (prime === test) {
+            console.log(prime);
+            break;
+        } else {
+            test++;
+        }
+    }
+    prime++;
+}
+
+// 2. Use a while loop to add up the numbers 1 to 20 into one variable. Print out the sum at the end
+let sum = 0;
+let num = 1;
+while (num <=20) {
+    sum += num;
+    if (num === 20) {
+        console.log('\n' + 'Sum = ' + sum + '\n');
+    }
+    num++;
+}
+
+// 3. Use a while loop to print out the even number from 1 to 20. (You'll need Modulus for this. And an IF Statement.)
+
+let checkEven = 1;
+while (checkEven <= 20) {
+    if (checkEven % 2 === 0) {
+        console.log(+ checkEven);
+    }
+    checkEven++;
+}
+
+// 4.While loops Fundamentals: https://csx.codesmith.io/units/precourse-part-2/challenge-while-loops-fundamentals
+let count = 2;
+
+while (count < 8) {
+  count++;
+}
+
+console.log('\n' + count); 
